7f11e4bcf14316ac83632914461c66fa830ff5c1
ndndnd24 <ndndnd24@gmail.com>
2023-05-15 11:56:40 +0200

    added calculating functions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,12 +3,106 @@ import "../Calculator.css";
 
 const Calculator = () => {
   const [displayValue, setDisplayValue] = useState("0");
+  const [previousValue, setPreviousValue] = useState(null);
+  const [operator, setOperator] = useState(null);
+  const [waitingForSecondOperand, setWaitingForSecondOperand] = useState(false);
+
+  const clearDisplay = () => {
+    setDisplayValue("0");
+    setPreviousValue(null);
+    setOperator(null);
+    setWaitingForSecondOperand(false);
+  };
 
   const inputDigit = (digit) => {
-    setDisplayValue((prevDisplay) => {
-      const display = prevDisplay === "0" ? String(digit) : prevDisplay + digit;
-      return display;
-    });
+    if (waitingForSecondOperand) {
+      setDisplayValue(String(digit));
+      setWaitingForSecondOperand(false);
+    } else {
+      setDisplayValue((prevDisplay) => {
+        const display =
+          prevDisplay === "0" ? String(digit) : prevDisplay + digit;
+        return display;
+      });
+    }
+  };
+
+  const inputDecimal = () => {
+    if (waitingForSecondOperand) {
+      setDisplayValue("0.");
+      setWaitingForSecondOperand(false);
+    } else if (!displayValue.includes(".")) {
+      setDisplayValue((prevDisplay) => prevDisplay + ".");
+    }
+  };
+
+  const toggleSign = () => {
+    setDisplayValue((prevDisplay) =>
+      prevDisplay.charAt(0) === "-" ? prevDisplay.slice(1) : "-" + prevDisplay
+    );
+  };
+
+  const performOperation = (nextOperator) => {
+    const inputValue = parseFloat(displayValue);
+
+    if (operator === null) {
+      setPreviousValue(inputValue);
+    } else {
+      const currentResult = parseFloat(previousValue);
+      let newResult;
+
+      switch (operator) {
+        case "+":
+          newResult = currentResult + inputValue;
+          break;
+        case "-":
+          newResult = currentResult - inputValue;
+          break;
+        case "*":
+          newResult = currentResult * inputValue;
+          break;
+        case "/":
+          newResult = currentResult / inputValue;
+          break;
+        case "x^y":
+          newResult = Math.pow(currentResult, inputValue);
+          break;
+        case "%":
+          newResult = currentResult * (inputValue / 100);
+          break;
+        default:
+          return;
+      }
+
+      setPreviousValue(newResult);
+      setDisplayValue(String(newResult));
+    }
+
+    setWaitingForSecondOperand(true);
+    setOperator(nextOperator);
+  };
+
+  const handleEqual = () => {
+    if (!operator) return;
+
+    const inputValue = parseFloat(displayValue);
+
+    if (waitingForSecondOperand && operator !== "√x") {
+      setOperator(null);
+      return;
+    }
+
+    if (operator === "√x") {
+      const result = Math.sqrt(inputValue);
+      setDisplayValue(String(result));
+      setPreviousValue(result);
+      setOperator(null);
+      setWaitingForSecondOperand(true);
+      return;
+    }
+
+    performOperation();
+    setOperator(null);
   };
 
   const formatDisplayValue = (value) => {
@@ -17,14 +111,33 @@ const Calculator = () => {
     return parts.join(".");
   };
 
+  const squareRoot = () => {
+    const currentResult = parseFloat(displayValue);
+    const newResult = Math.sqrt(currentResult);
+    setPreviousValue(newResult);
+    setDisplayValue(String(newResult));
+    setWaitingForSecondOperand(true);
+  };
+
   return (
     <div className="calculator">
       <div className="display">{formatDisplayValue(displayValue)}</div>
       <div className="keypad">
-        <button className="key key-ac">AC</button>
-        <button className="key">+/-</button>
-        <button className="key">%</button>
-        <button className="key">÷</button>
+        <button className="key key-ac" onClick={clearDisplay}>
+          AC
+        </button>
+        <button className="key" onClick={toggleSign}>
+          +/-
+        </button>
+        <button className="key" onClick={() => performOperation("%")}>
+          %
+        </button>
+        <button
+          className="key key-colored"
+          onClick={() => performOperation("/")}
+        >
+          ÷
+        </button>
         <button className="key" onClick={() => inputDigit(7)}>
           7
         </button>
@@ -34,7 +147,12 @@ const Calculator = () => {
         <button className="key" onClick={() => inputDigit(9)}>
           9
         </button>
-        <button className="key">x</button>
+        <button
+          className="key key-colored"
+          onClick={() => performOperation("*")}
+        >
+          x
+        </button>
         <button className="key" onClick={() => inputDigit(4)}>
           4
         </button>
@@ -44,7 +162,12 @@ const Calculator = () => {
         <button className="key" onClick={() => inputDigit(6)}>
           6
         </button>
-        <button className="key">-</button>
+        <button
+          className="key key-colored"
+          onClick={() => performOperation("-")}
+        >
+          -
+        </button>
         <button className="key" onClick={() => inputDigit(1)}>
           1
         </button>
@@ -54,16 +177,29 @@ const Calculator = () => {
         <button className="key" onClick={() => inputDigit(3)}>
           3
         </button>
-        <button className="key">+</button>
+        <button
+          className="key key-colored"
+          onClick={() => performOperation("+")}
+        >
+          +
+        </button>
         <button className="key" onClick={() => inputDigit(0)}>
           0
         </button>
-        <button className="key">.</button>
-        <button className="key">x^y</button>
-        <button className="key">√x</button>
+        <button className="key" onClick={() => inputDecimal()}>
+          .
+        </button>
+        <button className="key" onClick={() => performOperation("x^y")}>
+          x^y
+        </button>
+        <button className="key" onClick={() => squareRoot()}>
+          √x
+        </button>
         <button className="key">M+</button>
         <button className="key">M-</button>
-        <button className="key key-equal">=</button>
+        <button className="key key-equal" onClick={() => handleEqual()}>
+          =
+        </button>
       </div>
     </div>
   );
